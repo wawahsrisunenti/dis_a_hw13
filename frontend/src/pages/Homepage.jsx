072c1b90cb903fe8fea1ab06922022d7be0e9e2a
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-13 13:38:01 +0700

    style: change position navbar in Homepage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,12 +1,11 @@
-import { VStack } from "@chakra-ui/react";
+import { HStack, Center } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Books from "../components/Books";
 import { getAllBooks } from "../modules/fetch";
- 
-
 
 export default function Homepage() {
   const [books, setBooks] = useState([]);
+
   useEffect(() => {
     const fetchBooks = async () => {
       const books = await getAllBooks();
@@ -16,10 +15,12 @@ export default function Homepage() {
   }, []);
 
   return (
-    <VStack w="100vw">
-      {books?.books?.map((book) => (
-        <Books key={`${book.id} ${book.title}`} {...book} />
-      ))}
-    </VStack>
+    <Center>
+      <HStack spacing={4} mx={4} justifyContent="center">
+        {books?.books?.map((book) => (
+          <Books key={`${book.id} ${book.title}`} {...book} />
+        ))}
+      </HStack>
+    </Center>
   );
 }
