e086db1e71e7981e245de14edff4f829c63c22fb
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-13 14:35:59 +0700

    Feat: add login page and register

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,10 +11,42 @@ export default function Login() {
       navigate("/");
     }
   }, []);
-  
+
   return (
     <div>
       <h1>Login</h1>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          // http request
+          try {
+            const res = await axios.post('http://localhost:8000/login', {
+              email: e.target.email.value,
+              password: e.target.password.value
+            });
+            console.log("res", res);
+            // storing token in local storage
+            localStorage.setItem("token", res.data.token);
+            navigate("/");
+          }
+          catch (e) {
+            window.alert(e.response.data.message || "Something wrong");
+          }
+        }}
+        style={{
+          display: 'flex',
+          flexDirection: "column"
+        }}>
+        <label style={{ marginTop: 8, marginBottom: 8 }}>
+          Email:
+          <input required type="email" name="email"/>
+        </label>
+        <label required style={{ marginTop: 8, marginBottom: 8 }}>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 import {
   Box,
